42729ff6ef6a5bd560aa89c62e19912031a7f93a
Garrick Suemith <garrick.suemith@gmail.com>
2021-03-26 01:15:26 -0400

    removed nav component

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Nav from './components/Nav';
-import Header from './components/Header';
 
 ReactDOM.render(
   <React.StrictMode>
