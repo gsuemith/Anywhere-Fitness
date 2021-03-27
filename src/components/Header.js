f4194d8374d5ed6000d1bf8d6f1930295c7b4676
Garrick Suemith <garrick.suemith@gmail.com>
2021-03-27 02:58:44 -0400

    menu bar now reveals below banner

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Header = () => {
+  const [belowBanner, setBelowBanner] = useState(false)
 
+  useEffect(() => {
+    document.addEventListener('scroll', trackScrolling)
+  }, [])
+
+  const trackScrolling = () => {
+    const el = document.getElementById('banner');
+    if (el.getBoundingClientRect().bottom < 0) {
+      setBelowBanner(true)
+    } else {
+      setBelowBanner(false)
+    }
+  };
 
   return (
-  <header id="header" className="alt">
+  <header id="header" className={belowBanner ? "reveal" : "alt"}>
     <a href="/#" className="logo"><strong>AF</strong> <span>by TT16</span></a>
     <nav>
       <a href="#menu">Menu</a>
