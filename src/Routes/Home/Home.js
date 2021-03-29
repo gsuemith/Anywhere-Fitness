1eeb241904a61d8cc06dae3ec5ce5fadee5b0e1d
Garrick Suemith <garrick.suemith@gmail.com>
2021-03-29 05:47:12 -0400

    lowercase folder names

--- src/Routes/Home/Home.js
+++ src/Routes/Home/Home.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import Banner from './Banner'
-import Contact from './Contact/Contact'
-import PT from './PT'
-import Tiles from './Tiles'
+import Banner from './components/Banner'
+import Contact from './components/Contact/Contact'
+import PT from './components/PT'
+import Tiles from './components/Tiles'
 
 const Home = () => {
   return (
