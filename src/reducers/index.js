b0bcf1c6f50810cb9c701e982430c6368369f31d
Garrick Suemith <garrick.suemith@gmail.com>
2021-03-30 05:06:15 -0400

    added initial state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,25 @@
 const initialState = {
-
+  user: {},
+  instructors: [],
+  clients: [],
+  classes: [],
+  attending: [],
+  enrolling: [],
+  createClassForm: {
+    sport: '',
+    level: '',
+    location: '',
+    dateTime: '',
+    duration: 0,
+  },
+  searchForm: {
+    sport: '',
+    level: '',
+    location: '',
+    startDateTime: '',
+    endDateTime: '',
+    maxDuration: '',
+  },
 }
 
 const reducer = (state = initialState, { type, payload }) => {
