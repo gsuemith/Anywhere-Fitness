d0bc12ca21b823b7481c97808cdf0e9bfd84db0b
ErikBurdett <erikaburdet@gmail.com>
2021-03-31 10:52:24 -0500

    Worked on login form - had it showing but broke it somehow. Will fix soon

--- src/routes/login/Login.js
+++ src/routes/login/Login.js
@@ -1,10 +1,58 @@
-import React from "react";
+import React, {useState} from "react";
+
+const initialFormValues = {
+	email: "",
+	password: "",
+  };
+  
+  const initialFormErrors = {
+	email: "",
+	password: "",
+  };
+  const initialDisabled = true;
+  
+  function SignIn(props) {
+	const [disabled, setDisabled] = useState(initialDisabled);
+	const [formValues, setFormValues] = useState(initialFormValues);
+	const [formErrors, setFormErrors] = useState(initialFormErrors);
+  
+
 
 const Login = () => {
+	const [initialFormValues, setCredentials] = useState(initialState);
+	const handleChange = e => {
 
-	return( <form>
-		<h1>Login</h1>
-		
-		</form>);
+	}
+	return( 
+	<section>
+    <form method="post" action="#">
+      <div className="fields">
+        <div className="field half">
+          <label htmlFor="name">Email:</label>
+          <input type="text" name="email" id="email" value={formValues.email} />
+        </div>
+        <div className="field half">
+          <label htmlFor="email">Password:</label>
+          <input type="text" name="password" id="password" value={formValues.password} />
+        </div>
+        {/* <div className="field">
+          <label htmlFor="message">Message</label>
+          <textarea name="message" id="message" rows="6"></textarea>
+        </div> */}
+      </div>
+      {/* <ul className="actions">
+        <li><input type="submit" value="Send Message" className="primary" /></li>
+        <li><input type="reset" value="Clear" /></li>
+      </ul> */}
+    </form>
+  </section>)
+		// <form>
+		// <h1>Login</h1>
+		// <input type='text' name = "username" value = {credentials.username} onChange = {handleChange}/>
+		// </form>);
 };
 export default Login;
+
+// axios call to post credentials on submit
+// handle change function 
+// add submit button 
