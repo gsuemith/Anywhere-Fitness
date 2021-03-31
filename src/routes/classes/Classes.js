857c49c28bc59c3ad135ce8bd61a19d253734fe1
Wynstona Louis <wynstona@Wynstonas-MacBook-Pro.local>
2021-03-31 14:41:09 -0400

    done with cllass page will style more late, having problems with usehistory on Class.js needs a route to login if noot logged it

--- src/routes/classes/Classes.js
+++ src/routes/classes/Classes.js
@@ -1,32 +1,31 @@
-import React, { useEffect } from 'react'
-import { connect, useSelector } from 'react-redux'
+import React, { useEffect } from "react";
+import { connect, useSelector } from "react-redux";
 
-import Banner from './components/Banner'
-import SearchForm from './components/SearchForm'
-import Schedule from './components/Schedule'
+import Banner from "./components/Banner";
+import SearchForm from "./components/SearchForm";
+import Schedule from "./components/Schedule";
 
-import { getClasses } from '../../actions'
+import { getClasses } from "../../actions";
+import axios from "axios";
 
 const Classes = ({ getClasses }) => {
-  const classes = useSelector(state => state.classes)
-  const locations = useSelector(state => state.locations)
+	const classes = useSelector((state) => state.classes);
+	const locations = useSelector((state) => state.locations);
 
-  // fetch api classes
-  useEffect(() => {
-    if (classes.length === 0)
-      getClasses();
-  }, [getClasses, classes, locations])
+	// fetch api classes
+	useEffect(() => {
+		if (classes.length === 0) getClasses();
+	}, [getClasses, classes, locations]);
 
-  return (
-    <>
-      <Banner N={2}/>
-      <SearchForm />
-      <Schedule />
-    </>
-  )
-}
-
+	return (
+		<>
+			<Banner N={2} />
+			<SearchForm />
+			<Schedule classes={classes} />
+		</>
+	);
+};
 
 export default connect(null, {
-  getClasses
-})(Classes)
+	getClasses,
+})(Classes);
--- src/routes/login/Login.js
+++ src/routes/login/Login.js
@@ -1,10 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+// import "../login.css";
 
 const Login = () => {
+	const [state, setState] = useState({
+		username: "",
+		password: "",
+		errorMessage: "",
+	});
 
-	return( <form>
-		<h1>Login</h1>
-		
-		</form>);
+	const handleChange = (e) => {
+		setState({
+			...state,
+			[e.target.name]: e.target.value,
+		});
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (state.username === "" || state.password === "") {
+			return setState({
+				errorMessage: "Name, position and nickname fields are required.",
+			});
+		}
+	};
+
+	return (
+		<form classname="loginForm" onSubmit={handleSubmit}>
+			<div className="form-group">
+				<label className="username">Username: </label>
+				<br />
+				<input
+					type="text"
+					onChange={handleChange}
+					value={state.username}
+					name="username"
+					id="username"
+				/>
+			</div>
+			<div className="form-group">
+				<label className="password">Password: </label>
+				<br />
+				<input
+					type="password"
+					onChange={handleChange}
+					value={state.password}
+					name="password"
+					id="password"
+				/>
+			</div>
+			<button className="loginBTN" onSubmit={handleSubmit} type="submit">
+				Login
+			</button>
+		</form>
+	);
 };
 export default Login;
